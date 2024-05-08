9e346f076961c7e15d4d70ab81dcf8bcf4e4544b
dpuentex <dpuente1097@gmail.com>
2024-05-07 22:42:47 -0700

    Authentication

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,17 +1,24 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
+export const CurrentUser = createContext();
 
-export const CurrentUser = createContext()
-
-function CurrentUserProvider({ children }){
-
-    const [currentUser, setCurrentUser] = useState(null)
-
-    return (
-        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-            {children}
-        </CurrentUser.Provider>
-    )
+function CurrentUserProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    const getLoggedInUser = async () => {
+      let response = await fetch(
+        "http://localhost:3030/authentication/profile"
+      );
+      let user = await response.json();
+      setCurrentUser(user);
+    };
+    getLoggedInUser();
+  }, []);
+  return (
+    <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
+      {children}
+    </CurrentUser.Provider>
+  );
 }
 
-export default CurrentUserProvider
+export default CurrentUserProvider;
